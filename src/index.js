bfecf40fda5acf4273b60dfe214aabb258443061
Yangerle <1136510804@qq.com>
2018-10-24 15:15:41 +0800

    生产环境构建

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import Icon from './icon.jpg';
 import Data from './data.json'
 import printMe from './print.js';
 import { cube } from './math.js';
+if (process.env.NODE_ENV !== 'production'){
+    console.log('Looks like we are in development mode!');
+}
 function component() {
     var element = document.createElement('div');
     var btn = document.createElement('button');
